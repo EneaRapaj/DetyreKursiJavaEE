fbab0673c172ac029e5a38b7eb9343587e65b60c
menxanote <menxanoteroxario@gmail.com>
2024-05-26 14:36:16 +0200

    Detyre Kursi

---
+++ example_49/src/main/java/com/eazybytes/eazyschool/model/Response.java
@@ -0,0 +1,22 @@
+package com.eazybytes.eazyschool.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+
+    private String statusCode;
+    private String statusMsg;
+
+    public void setStatusCode(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public void setStatusMsg(String statusMsg) {
+        this.statusMsg = statusMsg;
+    }
+}
